9d76526406d280fc3141f089b63ae03ee6716cbd
MD Alam <alammd1989@gmail.com>
2019-11-08 23:45:39 -0500

    Update Config.java

--- src/base/Config.java
+++ src/base/Config.java
@@ -20,7 +20,7 @@ public void setupbrowser(String browserName) {
 		
 		if(browserName.equalsIgnoreCase("chrome")) {
 			//System.setProperty("webdriver.chrome.driver","/Users/jewel/git/MyFramework004/Drivers/chromedriver");
-			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver.exe");
+			System.setProperty("webdriver.chrome.driver",System.getProperty("user.dir")+"//Drivers//chromedriver");
 			driver = new ChromeDriver();
 		}
 		else if(browserName.equalsIgnoreCase("firefox")){
